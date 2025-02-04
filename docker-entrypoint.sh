e1e1abd3d48aa81466f7c241006321bf25811e6c
Oleg Zapara <ole9zp@gmail.com>
2025-02-04 14:49:27 +0200

    docker-entrypoint config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,13 @@
 set -e
 
 # Run migrations
-echo "Running database migrations..."
-diesel migration run
+if [ -z "$DATABASE_URL" ]; then
+    echo "DATABASE_URL is not set. Exiting."
+    exit 1
+fi
+
+# Run Diesel migrations
+diesel migration run --database-url "$DATABASE_URL"
 
 # Start application
 echo "Starting application..."
