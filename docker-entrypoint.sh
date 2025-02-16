0e74593e1f70ef8558fbf0ee8f89729b7606ef01
Oleg Zapara <ole9zp@gmail.com>
2025-02-16 15:52:12 +0200

    github worflow setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,10 +8,8 @@ if [ -z "$DATABASE_URL" ]; then
 fi
 
 # Run Diesel migrations
-echo "DATABASE_URL=$DATABASE_URL" > .env
 RUST_LOG=warn diesel setup || echo "Database already initialized"
 RUST_LOG=warn diesel migration run --database-url "$DATABASE_URL"
 
 # Start application
-echo "Starting application..."
 exec /app/evil_lumios
